528e2f5949616a0243367587f09b5c928c65343f
Sarthak Roy <sarthakroy2002@gmail.com>
2024-08-31 12:28:44 +0530

    Add Checks if Vibration is enabled
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- app/src/main/java/com/neolit/simplevibrationtest/MainActivity.java
+++ app/src/main/java/com/neolit/simplevibrationtest/MainActivity.java
@@ -1,14 +1,16 @@
 package com.neolit.simplevibrationtest;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.CombinedVibration;
 import android.os.VibrationEffect;
 import android.os.VibratorManager;
+import android.provider.Settings;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
@@ -19,7 +21,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "Make sure to Enable Touch feedback!", Toast.LENGTH_SHORT).show();
+
+        promptToEnableHapticFeedback(this);
 
         final VibratorManager vibrator = (VibratorManager) getSystemService(Context.VIBRATOR_MANAGER_SERVICE);
 
@@ -73,4 +76,35 @@ public class MainActivity extends AppCompatActivity {
                 builder.show();
         });
     }
+
+    public boolean isVibrationEnabled(Context context) {
+        return Settings.System.getInt(context.getContentResolver(),
+                Settings.System.VIBRATE_ON, 0) == 1;
+    }
+
+    public boolean isHapticFeedbackEnabled(Context context) {
+        return Settings.System.getInt(context.getContentResolver(),
+                Settings.System.HAPTIC_FEEDBACK_ENABLED, 0) == 1;
+    }
+
+    public void promptToEnableHapticFeedback(Context context) {
+        if (!isVibrationEnabled(context) || !isHapticFeedbackEnabled(context)) {
+            MaterialAlertDialogBuilder builder = getMaterialAlertDialogBuilder();
+            builder.show();
+        }
+    }
+
+    private @NonNull MaterialAlertDialogBuilder getMaterialAlertDialogBuilder() {
+        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(MainActivity.this);
+        builder.setMessage("Touch vibration is currently disabled. Would you like to enable it?");
+        builder.setTitle("Enable Touch Vibration");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Go to Settings", (dialog, which) -> {
+            Intent intent = new Intent(Settings.ACTION_SOUND_SETTINGS);
+            startActivity(intent);
+        });
+        builder.setNegativeButton("No", (dialog, which) -> finish());
+        return builder;
+    }
+
 }
